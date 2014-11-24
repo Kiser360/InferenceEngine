6a912ea80a780dc28302a73f43f8ea37193353e9
Kiser360 <Kiser360@gmail.com>
2014-11-24 14:37:31 -0600

    Initial Commit
    Includes a little code to include SQLite library.  Creates an SQLite DB
    if non exists in the directory.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.IO;
+
+namespace InferenceEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string DEF_DB = "MyDatabase.sqlite";
+
+            //Lets see if we need to create the DB
+            if (!File.Exists(DEF_DB))
+            {
+                SQLiteConnection.CreateFile(DEF_DB);
+                Console.WriteLine("Creating DB for the first time");
+            }
+
+
+
+            //I NEED TIME TO READ THE CONSOLE!!!!
+            //    **This pauses execution**
+            Console.Read();
+        }
+    }
+}
